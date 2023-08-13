e01e0593bdcc9a0785345bbb8f0606fab886f205
Duckcanfly <duckcanfly@Duckcanflys-MacBook-Pro.local>
2023-08-14 06:45:16 +0700

    tentang tugas

--- README.md
+++ README.md
@@ -9,8 +9,8 @@ git commit -m "pesannya(boleh apa aja)"
 git push origin <branch yang dibuat>
 
 ## buat branch baru
-git branch <nama branch> atau git checkout -b <nama branch>
-git checkout <nama branch>
+git branch <nama branch>(hanya buat branch) atau git checkout -b <nama branch>(buat branch dan langsung pindah branch)
+git checkout <nama branch> (hanya pindah branch)
 
 ## tarik tugas
 git pull origin <nama branch>
--- tugas.js
+++ tugas.js
@@ -1,2 +1,20 @@
 // soal 1
 // jika terdapat data dengan nama dan nilai ujian murid dari skala 0-100, berapa jumlah murid yang dinyatakan lulus dan tidak lulus jika batas minimum kelulusannya adalah 75. Jika nilai siswa sama dengan atau lebih besar dari 75 maka siswa dinyatakan lulus, tetapi jika nilai siswa di bawah 75 maka dinyatakan tidak lulus. buat algoritma dan codingannya
+
+// bikin algoritma dibawah ini pakai comment
+
+// data test
+const users = [
+  { name: "Alice", score: 85 },
+  { name: "Bob", score: 72 },
+  { name: "Charlie", score: 95 },
+  { name: "David", score: 64 },
+  { name: "Eva", score: 78 },
+  { name: "Frank", score: 91 },
+  { name: "Grace", score: 89 },
+  { name: "Helen", score: 67 },
+  { name: "Ivan", score: 76 },
+  { name: "Jack", score: 83 },
+];
+
+// tulis codingan di bawah ini menggunakan data test diatas
