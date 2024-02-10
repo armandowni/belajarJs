a5f25efe71e55ab19ac0e801558be96bd7f1fc37
Duckcanfly <armando.nurimansyah@gmail.com>
2024-02-10 21:55:14 +0700

    fixing pert01

--- pert01.js
+++ pert01.js
@@ -4,17 +4,44 @@ console.log("====================================");
 
 console.log("Hello World");
 
-var a = 10;
-let b = 20;
-const c = 30;
+// non-civilized
+// string 'alip20'
+// number 20 10000000000000
+// boolean true/false
 
+// civilized
+// array ['alip20',20.true]
+// object {nama:'alip',umur:20}
+
+// x + 10 = 15, kalau x = 5
+// f(5) = 5 + 15 = 20
+
+// selection if, else if, else, switch&case
+// looping for ,for of , for in, do while, while
+
+// variable
+var a = 10;
+let b = a;
+const c = a;
+
+const akajskhd = "alip20";
+const boolean = true;
+b = "armando";
+
+console.log(a, b, c);
 if (true) {
-  var a = 40;
-  let b = 5;
+  a = 40;
+  b = 5;
   const c = 20;
   // 40,5,30
   console.log("Hasil 1 : ", a, b, c);
 }
 
-// 40,20,30
-console.log("Hasil 2 : ", a, b, c);
+// function
+function test() {
+  b = "alip";
+  return 5 + 15;
+}
+
+console.log(test());
+console.log("Hasil 3 : ", a, b, c);
